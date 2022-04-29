ee2549b32be1307359509bdc23fc81e046c73606
Mirza85 <mirzaaliarsyad85@gmail.com>
2022-04-29 23:39:17 +0700

    rabbit first

---
+++ consumer.js
@@ -0,0 +1,20 @@
+const amqp = require('amqplib');
+
+
+const init = async() => {
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    const queue = 'dicoding';
+
+    await channel.assertQueue(queue, {
+        durable: true,
+    });
+    channel.consume(queue, (message) => {
+        console.log(`menerima pesan dari queue ${queue}: ${message.content.toString()}`);
+    }, { noAck: true })
+};
+
+
+
+init();
